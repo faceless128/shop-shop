688b31da1817e05643a271f672d39b257055c4af
Kaijam Jeffers <faceless128@gmail.com>
2022-09-17 00:59:24 -0400

    updated detail to remove from cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -43,6 +43,13 @@ function Detail() {
     }
   };
 
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+  };
+
   return (
     <>
       {currentProduct ? (
@@ -56,7 +63,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
