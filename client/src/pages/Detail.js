a65c7f7528028ae2881f2fb9123050fc6a8c8b1c
Kaijam Jeffers <faceless128@gmail.com>
2022-09-17 00:41:16 -0400

    updated detail

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,7 +5,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS, } from "../utils/actions";
 import Cart from '../components/Cart';
 
 function Detail() {
@@ -14,7 +14,7 @@ function Detail() {
   const [currentProduct, setCurrentProduct] = useState({})
   const { loading, data } = useQuery(QUERY_PRODUCTS);
   const { products } = state;
-  
+
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
@@ -25,6 +25,13 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
 
   return (
     <>
@@ -38,7 +45,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to cart</button>
             <button>Remove from Cart</button>
           </p>
 
